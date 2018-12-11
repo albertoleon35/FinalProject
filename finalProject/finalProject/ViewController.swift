de04a9be33721062b66e0d33a1041325b06a4154
Alberto Leon <albertoleon325@gmail.com>
2018-12-10 19:57:46 -0800

    project skeleton

---
+++ finalProject/finalProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  finalProject
+//
+//  Created by Alberto Leon on 12/10/18.
+//  Copyright © 2018 Alberto Leon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
